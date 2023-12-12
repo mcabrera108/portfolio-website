6241144035eb90f3359d7f0fc648ceef1dbd99ec
Martin Cabrera <martin.cabrera103@gmail.com>
2023-12-12 05:45:32 -0600

    Added About Section andfunctionality to nav buttons

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@
 
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("navigation")[0];
+const homeButton = document.querySelector("#home-btn");
+const aboutButton = document.querySelector("#about-btn");
 
 toggleButton.addEventListener("click", () => {
     navbarLinks.classList.toggle("active");
 });
-
+homeButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.querySelector("#home").scrollIntoView();
+})
+aboutButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.querySelector("#about").scrollIntoView();
+})
